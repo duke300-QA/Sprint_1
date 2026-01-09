9a8da06e57eea8e0615ff84e6deb7d4924840960
duke300-QA <duke3001986@gmail.com>
2026-01-10 00:05:59 +0300

    Reformat code structure in task_2.py

--- task_2.py
+++ task_2.py
@@ -3,7 +3,6 @@ class Tester:
     def __init__(self, name):
         self.name = name  
         self.deadline = True  
-
     def work_hard(self, deadline=True):
         if deadline:
             print(self.name, 'Что ж, ещё часок поработаю!')
@@ -14,3 +13,4 @@ tester_1 = Tester(name='tester_1')
 tester_1.work_hard(deadline=False)  # 'tester_1 Можно отдыхать'
 tester_2 = Tester(name='tester_2')
 tester_2.work_hard(deadline=True)   # 'tester_2 Что ж, ещё часок поработаю!'
+
