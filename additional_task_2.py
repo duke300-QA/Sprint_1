c0f05621faee36294164c3002bee041a29eb06ab
duke300-QA <duke3001986@gmail.com>
2026-01-10 00:03:00 +0300

    Refactor digit_root function and add test case
    Reformatted the digit_root function and added a new print statement.

--- additional_task_2.py
+++ additional_task_2.py
@@ -2,6 +2,7 @@ def digit_root(num):
     if num < 10:
         return num
     return digit_root(sum(int(digit) for digit in str(num)))
+    
 print(digit_root(4851))
 print(digit_root(97569))
 print(digit_root(889987))
